493aa8fe3486acd01be6ee9e3f5f65abb231ef2e
kledenai <bruno.silva1643@etec.sp.gov.br>
2020-10-16 06:49:38 -0300

    feat: adding styled-components and config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,34 @@
-import Head from 'next/head'
+import { useState, useEffect } from 'react';
+import { Title } from '../styles/pages/Home';
+
+interface IProduct {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => {
+        setRecommendedProducts(data);
+      })
+    })
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <section>
+        <Title>Hello World</Title>
+        {recommendedProducts.map(product => {
+          return(
+            <li key={product.id}>
+              {product.title}
+            </li>
+          );
+        })}
+      </section>
     </div>
   )
 }
